b56f6a3dbeb09ccc1221584a399523735947796f
sai kumar <arvetisaikumar52@gmail.com>
2025-05-12 18:06:41 +0530

    ini

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Donate from "./pages/Donate";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="flex justify-center items-center h-screen font-bold">
-        Upliftaid
-      </h1>
+      <Donate />
     </>
   );
 }
---
+++ src/pages/Donate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Donate = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default Donate
