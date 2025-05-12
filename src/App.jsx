168f1639391867c5940ba4dc4234f4f283439c95
prabhash1475 <prabhash.kumar96@gmail.com>
2025-05-12 14:36:51 +0530

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="flex justify-center items-center h-screen font-bold">
+        Upliftaid
+      </h1>
+    </>
+  );
+}
+
+export default App;
