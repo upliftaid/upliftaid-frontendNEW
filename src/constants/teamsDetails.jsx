90515a04bf83068f3367e3f7cabb436575cce6d0
HarshGupta <harshgupta88911@gmail.com>
2025-06-05 12:13:04 +0530

    create: team detail array of object

---
+++ src/constants/teamsDetails.jsx
@@ -0,0 +1,63 @@
+import { FaFacebook, FaTwitter, FaGoogle, FaPinterest } from "react-icons/fa";
+export const team = [
+  {
+    id: 1,
+    name: "Phillip Parisis",
+    role: "Senior Worker",
+    description:
+      " The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those intereste.",
+    image:
+      "https://zozothemes.com/html/the-charity/img/sections/team/volunteer1.jpg",
+    socialIcons: [
+      <FaFacebook key="fb" />,
+      <FaTwitter key="tw" />,
+      <FaGoogle key="go" />,
+      <FaPinterest key="pin" />,
+    ],
+  },
+  {
+    id: 2,
+    name: "Fruklein",
+    role: "Senior Worker",
+    description:
+      " The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those intereste.",
+    image:
+      "https://zozothemes.com/html/the-charity/img/sections/team/volunteer2.jpg",
+    socialIcons: [
+      <FaFacebook key="fb" />,
+      <FaTwitter key="tw" />,
+      <FaGoogle key="go" />,
+      <FaPinterest key="pin" />,
+    ],
+  },
+  {
+    id: 3,
+    name: "Kimberle",
+    role: "Senior Worker",
+    description:
+      " The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those intereste.",
+    image:
+      "https://zozothemes.com/html/the-charity/img/sections/team/volunteer3.jpg",
+    socialIcons: [
+      <FaFacebook key="fb" />,
+      <FaTwitter key="tw" />,
+      <FaGoogle key="go" />,
+      <FaPinterest key="pin" />,
+    ],
+  },
+  {
+    id: 4,
+    name: "Mariya",
+    role: "Senior Worker",
+    description:
+      " The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those intereste.",
+    image:
+      "https://zozothemes.com/html/the-charity/img/sections/team/volunteer4.jpg",
+    socialIcons: [
+      <FaFacebook key="fb" />,
+      <FaTwitter key="tw" />,
+      <FaGoogle key="go" />,
+      <FaPinterest key="pin" />,
+    ],
+  },
+];
