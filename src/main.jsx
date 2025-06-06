bcadab38c1e7afb1c7cce4ac5ba026506569a179
Rohit Kumar <kumarguptarohit2005@gmail.com>
2025-06-06 17:39:32 +0530

    Routes Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/about" element={<AboutUsPage /> } />
+        <Route path="/about/our-story" element={<OurStoryPage /> } />
+        <Route path="/about/what-we-do" element={<WhatWeDoPage /> } />
+        <Route path="/about/be-a-volunteer" element={<BeAVolunteerPage /> } />
+        <Route path="/about/faq" element={<FAQPage /> } />
+        <Route path="/about/testimonals" element={<TestimonalsPage /> } />
+        <Route path="/about/gallery" element={<GalleryPage /> } />
+        <Route path="/causes" element={<CausesPage /> } />
+        <Route path="/events" element={<EventsPage /> } />
+        <Route path="/portfolio" element={<PortfolioPage /> } />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/contact" element={<ContactUsPage/> } />
+        <Route path="/donate" element={<ContactPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
