8eaae9a537e2eaae171c304b9c5ff50db4f22c8b
Rohit Kumar <kumarguptarohit2005@gmail.com>
2025-07-03 16:10:02 +0530

    Event Page completed

---
+++ src/components/EventsDetails.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+
+const EventDetail = () => {
+
+const [formData , setFormData] = useState({
+  fullName : '',
+  phone: '',
+  email : '',
+  message : ''
+})
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  console.log("fromData", formData)
+  setFormData({
+      fullName : '',
+  email : '',
+  message : ''
+  })
+}
+
+  return (
+    <div className="w-full p-4 grid gap-4 h-fit">
+        <h1 className="font-medium text-[26px]">FEED A HUNGRY CHILD</h1>
+          <div>
+            <img src="https://zozothemes.com/html/the-charity/img/sections/blog/single-blog.jpg" alt="image" />
+          </div>
+          <div className="grid gap-6">
+            <div className="grid gap-1">
+              <h1 className="text-[24px] font-medium">EVENT DESCRIPTION</h1>
+              <p className="text-justify text-[14px]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus dolores similique voluptas recusandae! Natus beatae eum quo quisquam expedita, est ratione vero qui officia dolores ad facere a aut. Nemo sint, iste dolorum blanditiis nam sapiente fugiat temporibus accusantium ipsum minus libero nesciunt ducimus unde cum deleniti suscipit inventore nobis.</p>
+            </div>
+            <div className="grid">
+              <p className="font-medium text-[24px]">EVENT CONTENT</p>
+              <ul className="list-disc p-4 px-6 text-[14px] grid gap-2">
+                <li>100 volunteers needed for this charity activity.</li>
+                <li>We aggregate in the city center at 8:00 pm.</li>
+                <li>Everyone will get items and we divided into 2 group to start to 2 directions.</li>
+                <li>We will go to the place where homeless usually stay and give items for them.</li>
+                <li>Every group has a leader and a secretary to note important problems what we will ask for help from nonprofits organization or NGOs.</li>
+                <li>NOTE: Everyone ensure safety and punctuality.</li>
+              </ul>
+            </div>
+            <hr className="text-gray-300 rounded"/>
+            <div className="grid gap-4">
+              <h1 className="font-medium text-[24px]">Leave A Reply</h1>
+              <form action="" className="grid gap-4 place-items-start" onSubmit={handleSubmit}>
+                <div>
+                  <input 
+                    type="text" 
+                    name="fullName" 
+                    id="fullName" 
+                    placeholder="Full Name"
+                    value={formData.fullName} 
+                    onChange={(e) => setFormData({...formData , fullName:e.target.value})}
+                    className="border border-gray-300 rounded p-2 outline-none text-[14px] lg:w-[800px] min-w-70 md:w-[700px]"
+                  />
+                  </div>
+                  <div>
+                    <input 
+                        type="text" 
+                        name="phone" 
+                        id="phone" 
+                        placeholder="Phone"
+                        value={formData.phone} 
+                        onChange={(e) => setFormData({...formData , phone:e.target.value})}
+                        className="border border-gray-300 rounded p-2 outline-none text-[14px] lg:w-[800px] min-w-70 md:w-[700px]"
+                    />
+                </div>
+                <div>
+                  <input 
+                    type="text" 
+                    name="email" 
+                    id="email" 
+                    placeholder="Email"
+                    value={formData.email} 
+                    onChange={(e) => setFormData({...formData , email :e.target.value})}
+                    className="border border-gray-300 rounded p-2 outline-none text-[14px] lg:w-[800px] min-w-70 md:w-[700px]"
+                  />
+                </div>
+                <div>
+                  <textarea 
+                    type="text" 
+                    name="message" 
+                    id="message" 
+                    placeholder="Message"
+                    value={formData.message} 
+                    onChange={(e) => setFormData({...formData , message :e.target.value})}
+                    className="border border-gray-300 rounded p-2 outline-none text-[14px] lg:w-[800px] md:w-[700px] min-w-70"
+                  />
+                </div>
+                <button className="hover:bg-[#E69F00] rounded hover:text-white cursor-pointer px-4 py-2 bg-[#FFB204] ">Book Now</button>
+              </form>
+            </div>
+
+          </div>
+        </div>
+  )
+}
+
+export default EventDetail
--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,27 +1,18 @@
-import { useState } from "react";
 import Navbar from "../layout/Navbar";
 import Footer from "../layout/Footer";
 import MobileNavBar from "../layout/MobileNavBar";
 import events from "../constants/events";
-import MainEventsSection from "../components/MainEventsSection";
 import UpcomingEventsSection from "../components/UpcomingEventsSection";
 import BlogCategories from "../components/BlogCategories";
 import PopularPosts from "../components/PopularPosts";
-import EventModel from "../components/EventModel";
 import ArchivesDate from "../components/ArchivesDate";
 import TagsSection from "../components/TagsSection";
+import EventDetail from "../components/EventsDetails";
 
 export default function EventPage() {
-  const [activeTab, setActiveTab] = useState("Happening");
-  const [selectedEvent, setSelectedEvent] = useState(null);
-
-  // Filter events based on active tab
-  const filteredEvents = events.filter((event) => event.status === activeTab);
 
   // Filter upcoming events for sidebar
   const upcomingEvents = events.filter((event) => event.status === "Upcoming");
-
-  const tabs = ["Happening", "Upcoming", "Expired"];
 
   return (
     <div className="font-[Poppins]">
@@ -32,22 +23,7 @@ export default function EventPage() {
           <h1 className="text-center">Events</h1>
         </div>
         <div className="flex flex-col lg:flex-row lg:px-20 lg:gap-10">
-          <div className="grid w-full sm:px-8 px-2 py-20 gap-4">
-            {!selectedEvent ? (
-              <MainEventsSection
-                activeTab={activeTab}
-                setActiveTab={setActiveTab}
-                filteredEvents={filteredEvents}
-                tabs={tabs}
-                onEventClick={setSelectedEvent}
-              />
-            ) : (
-              <EventModel
-                setSelectedEvent={setSelectedEvent}
-                selectedEvent={selectedEvent}
-              />
-            )}
-          </div>
+            <EventDetail/>
             <aside className="flex flex-1/4 flex-col gap-10 px-5 pb-5 lg:py-20">
               <UpcomingEventsSection upcomingEvents={upcomingEvents} />
               <BlogCategories />
