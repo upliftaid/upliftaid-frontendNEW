ead8c35585e0fe79b36197f0105130cd2e5e7b95
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-07-28 18:30:49 +0530

    update the code for merging

--- src/pages/About_us.jsx
+++ src/pages/About_us.jsx
@@ -1,10 +1,10 @@
 import Navbar from '../layout/Navbar'
 import Footer from '../layout/Footer'
-import AboutUsSection from '../components/OurStorySection'
-import OurStory from '../components/OurStory'
-import Who_we_are from '../components/Who_we_are'
-import Meet_team from '../components/Meet_team'
-import Testimonials from '../components/Testimonals'
+import AboutUsSection from '../components/aboutUs/ourStory/OurStorySection'
+import OurStory from '../components/aboutUs/ourStory/OurStory'
+import Who_we_are from '../components/aboutUs/ourStory/Who_we_are'
+import Meet_team from '../components/aboutUs/ourStory/Meet_team'
+import Testimonials from '../components/aboutUs/ourStory/Testimonals'
 import MobileNavBar from '../layout/MobileNavBar'
 
 const About_us = () => {
