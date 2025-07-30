fab832296c7aba2c269d8878c6052f6232cebfdc
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-07-30 15:00:26 +0530

    update the code for merging

--- src/components/profile/Activity.jsx
+++ src/components/profile/Activity.jsx
@@ -1,8 +1,25 @@
 import { CiHeart } from "react-icons/ci";
 import { FaClock } from "react-icons/fa";
 import { FaCalendar } from "react-icons/fa";
-import ActivityDetails from "../constants/ActivityDetails";
+import { FaHandHoldingHeart } from "react-icons/fa6";
+import { PiUsersThree } from "react-icons/pi";
 
+const ActivityDetails = [
+    {
+        iconColor : 'green-500',
+        icon : FaHandHoldingHeart,
+        heading : 'Donated ₹1500 to Clean Water Project',
+        date : 'March 15, 2025',
+        amount : '₹1500' 
+    },
+    {
+        iconColor : 'blue-500',
+        icon : PiUsersThree,
+        heading : 'Volunteered at Food Distrubtion Event',
+        date : 'March 10, 2025',
+        amount : '6 hrs'
+    }
+]
 
 const Activity = () => {
   return (
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,10 @@
 import Navbar from "../layout/Navbar";
 import Footer from "../layout/Footer";
 import MobileNavbar from "../layout/MobileNavBar";
-import PersonalInfo from "../components/PersonalInfo";
-import Activity from "../components/Activity";
-import Settings from "../components/Seetings";
-import TopBar from '../components/TopBar'
+import PersonalInfo from "../components/profile/PersonalInfo";
+import Activity from "../components/profile/Activity";
+import Settings from "../components/profile/Seetings";
+import TopBar from '../components/profile/TopBar'
 
 const ProfilePage = () => {
   return (
