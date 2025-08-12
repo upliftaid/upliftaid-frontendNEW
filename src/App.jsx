b0f8f064841ab63fd89fc8cd768ab30442c4609e
Vakacharla-Lokesh <lokeshgpta04@gmail.com>
2025-08-12 22:22:11 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import EventPage from "./pages/EventPage";
 import Notfound from "./pages/Notfound";
 import HomePage from "./pages/HomePage";
@@ -31,7 +36,7 @@ import ProjectDetail from "./pages/ProjectDetail";
 // import Layout from "./components/Product2/Layout";
 import ProductsPage from "./pages/Product/ProductsPage";
 import CartPage from "./pages/Product/CartPage";
-import { store } from '../src/redux/store'
+import { store } from "../src/redux/store";
 import { Provider } from "react-redux";
 import DonationPage from "./pages/DonationPage";
 
@@ -39,51 +44,123 @@ function App() {
   const pathname = window.location.pathname;
   return (
     <>
-        <Router>
-              <Provider store={store}>
-          
-          <Routes >
-            <Route path="/" element={<Root/>}>
-              <Route path="/" element={<HomePage />}/>
-              <Route path="/events" element={<EventPage />}/>
-              <Route path="*" element={<Notfound />}/>
-              <Route path="/about-us" element={<About_us />}/>
-              <Route path="/about-us/be-a-volunteer" element={<BeAVolunteer />}/>
-              <Route path="/about-us/our-story" element={<Our_story />}/>
-              <Route path="/about-us/what-we-do" element={<WhatWeDoPage />}/>
-              <Route path="/about-us/faq" element={<FAQPage />}/>
-              <Route path="/contact" element={<ContactPage />}/>
-              <Route path="/career" element={<JoinTeamPage />}/>
-              <Route path="/about-us/testimonals" element={<Testimonials />}/>
-              <Route path="/causes" element={<CausePage />}/>
-              <Route path="/blog" element={<Blog />}/>
-              <Route path="/blog/:id" element={<BlogDetailPage />}/>
-              <Route path="/profile" element={<ProfilePage />}/>
+      <Router>
+        <Provider store={store}>
+          <Routes>
+            <Route
+              path="/"
+              element={<Root />}
+            >
+              <Route
+                path="/"
+                element={<HomePage />}
+              />
+              <Route
+                path="/events"
+                element={<EventPage />}
+              />
+              <Route
+                path="*"
+                element={<Notfound />}
+              />
+              <Route
+                path="/about-us"
+                element={<About_us />}
+              />
+              <Route
+                path="/about-us/be-a-volunteer"
+                element={<BeAVolunteer />}
+              />
+              <Route
+                path="/about-us/our-story"
+                element={<Our_story />}
+              />
+              <Route
+                path="/about-us/what-we-do"
+                element={<WhatWeDoPage />}
+              />
+              <Route
+                path="/about-us/faq"
+                element={<FAQPage />}
+              />
+              <Route
+                path="/contact"
+                element={<ContactPage />}
+              />
+              <Route
+                path="/career"
+                element={<JoinTeamPage />}
+              />
+              <Route
+                path="/about-us/testimonals"
+                element={<Testimonials />}
+              />
+              <Route
+                path="/causes"
+                element={<CausePage />}
+              />
+              <Route
+                path="/blog"
+                element={<Blog />}
+              />
+              <Route
+                path="/blog/:id"
+                element={<BlogDetailPage />}
+              />
+              <Route
+                path="/profile"
+                element={<ProfilePage />}
+              />
               {/* <Route path="/product-detail" element={<ProductDetailPage />}/> */}
               {/* <Route path="/cart" element={<ProductCart />}/> */}
               {/* <Route path="/product" element={<ProductPage />}/> */}
-              <Route path="/portfolio" element={<Portfolio />}/>
-              <Route path="/portfolio/:slug" element={<ProjectDetail />}/>
-              <Route path="/product" element={<ProductsPage />} />
-              <Route path="/product/:id" element={<ProductDetailPage />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/donate" element={<DonationPage />}/>
+              <Route
+                path="/portfolio"
+                element={<Portfolio />}
+              />
+              <Route
+                path="/portfolio/:slug"
+                element={<ProjectDetail />}
+              />
+              <Route
+                path="/product"
+                element={<ProductsPage />}
+              />
+              <Route
+                path="/product/:id"
+                element={<ProductDetailPage />}
+              />
+              <Route
+                path="/cart"
+                element={<CartPage />}
+              />
+              <Route
+                path="/donate"
+                element={<DonationPage />}
+              />
 
               {/* <Route path="/" element={<Navigate to="/products" replace />} /> */}
-      
-        
-
             </Route>
 
-            <Route path="/auth" element={<LoginForm />}/>
-            <Route path="/signup" element={<SignupForm />}/>
-            <Route path="/forget-password" element={<ForgetPassword />}/>
-            <Route path="/reset-password" element={<ResetPassword />}/>
+            <Route
+              path="/auth"
+              element={<LoginForm />}
+            />
+            <Route
+              path="/signup"
+              element={<SignupForm />}
+            />
+            <Route
+              path="/forget-password"
+              element={<ForgetPassword />}
+            />
+            <Route
+              path="/reset-password"
+              element={<ResetPassword />}
+            />
           </Routes>
-              </Provider>
-          
-        </Router>
-      
+        </Provider>
+      </Router>
     </>
   );
 }
--- src/root.jsx
+++ src/root.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
+import React from "react";
 import Navbar from "./layout/Navbar";
 import MobileNavbar from "./layout/MobileNavBar";
 import Footer from "./layout/Footer";
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 import ScrollToTop from "./layout/ScrollToTop";
 
 export default function Root() {
   return (
     <>
-    <ScrollToTop />
-    <Navbar />
-    <MobileNavbar />
-
-    <Outlet/>
-
-    <Footer />
-      
+      <ScrollToTop />
+      <Navbar />
+      <MobileNavbar />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
